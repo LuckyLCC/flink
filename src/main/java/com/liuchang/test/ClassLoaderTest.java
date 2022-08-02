03f9be47b1c6d4e5fbc655e07d490ffd686380cb
liuchang <liuchang@yahoo.com>
2022-08-02 22:25:24 +0800

    2222

--- src/main/java/com/liuchang/test/ClassLoaderTest.java
+++ src/main/java/com/liuchang/test/ClassLoaderTest.java
@@ -13,7 +13,7 @@ public class ClassLoaderTest {
 
         wwwwwwe
                 eqweqweqweq
-                qeqweqwqwe7788686ugjggikj
+                qeqweqwqwe7788686ugjggikj123321
     }
 
 }
