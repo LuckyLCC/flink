9b8e9ab00301af3cfa51002180d1d85d9ee351de
liuchang <liuchang@yahoo.com>
2022-08-03 00:04:31 +0800

    www

--- src/main/java/com/liuchang/test/ClassLoaderTest.java
+++ src/main/java/com/liuchang/test/ClassLoaderTest.java
@@ -10,7 +10,6 @@ public class ClassLoaderTest {
     public static void main(String[] args) {
         ClassLoaderTest.class.getResourceAsStream("/properties/globalConfig.properties");
         ClassLoaderTest.class.getClassLoader().getResourceAsStream("properties/globalConfig.properties");
-
     }
 
 }
